d7ee9a1246efa9beb0660ee29aec4fda74cfac90
VeronikaHorovenko <166665432+VeronikaHorovenko@users.noreply.github.com>
2025-09-25 17:09:08 -0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Veronika Horovenko on 25.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
